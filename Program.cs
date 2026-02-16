6c46255294dcba9a388a1db0e78dcd2484305f3c
selva-d3 <selvakumar.thiruppathi@d3minds.com>
2026-02-16 20:12:39 +0530

    Selva's function added 1

--- Program.cs
+++ Program.cs
@@ -2,14 +2,10 @@
 using System.IO;
 
 class Program
-{
-   static void Mains()
-   {
-       Console.Write("hello santoshhhhhhhhhhhhhh"); 
-   }
+{   
    static void PrintSelva()
    {
-       Console.Write("hello Selva"); 
+       Console.Write("hello Selvakumar"); 
    }
 
 }

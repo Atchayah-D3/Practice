fcadb427f6a156ea10b9b73c6e7cf7cac32258bf
HirthickD3 <hirthick.sivashanmugam@d3minds.com>
2026-02-16 17:14:16 +0530

    resolving

--- Program.cs
+++ Program.cs
@@ -5,9 +5,7 @@ class Program
 {
    static void Mains()
    {
-<<<<<<< HEAD
-       Console.Write("hello from Main");
-=======
+
        Console.Write("hello from Hirthick");
 >>>>>>> hirthick
         
